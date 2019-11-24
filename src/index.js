517b0097712c4efa15d5b6b9824a574fd6e8e987
Saúl Navarrov <Sinavarrov@gmail.com>
2019-11-24 10:39:05 -0500

    agregando datos al elemento items list

--- src/index.js
+++ src/index.js
@@ -7,6 +7,31 @@ const itemDom = $('#item');
 
 // funcion para actualizar listas
 function actualizarLista(items) {
+  // limpio el listado
+  itemsListDom.html('');
+
+  // iteramos en todos los items
+  for (const item of items) {
+    // clonamos el listado de item
+    const cloneItemDom = itemDom.clone();
+    // seleccionamos los elementos
+    const chkHabilitacdoDom = cloneItemDom.find('input');
+    const btnBorrarDom = cloneItemDom.find('button');
+    const lblNombreDom = cloneItemDom.find('span');
+
+    // Quitamos la clase que borra el elemento
+    cloneItemDom.removeClass('d-none');
+
+    // agregamos el texto al DOM
+    lblNombreDom.html('item.text');
+
+    // Iteramos si esta completado o no
+    if (item.completado) {
+      lblNombreDom.css('text-decoration','line-through');
+    }
+
+
+  }
 }
 
 
