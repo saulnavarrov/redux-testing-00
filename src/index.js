768d8a9a393e5d187f2ff6fc4cf5a30f08c820b6
Saúl Navarrov <Sinavarrov@gmail.com>
2019-11-24 09:59:27 -0500

    trabajando en las funciones
    se agrega una subcripcion del las storas y organizar los cambios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Probando scripts');
+// importando la store creadas de redux
+import store from './redux/store';
+
+// seleccionamos los elementos de htmlDom
+const itemsListDom = $('#itemList');
+const itemDom = $('#item');
+
+// funcion para actualizar listas
+function actualizarLista(items) {
+}
+
+
+// cualquier cambio se vea reflejado en la pantalla
+store.subcribe(() => {
+  const state = store =getState();
+
+  // defino una funcion para actualizar la listados
+  // envio los stados
+  actualizarLista(state);
+});
