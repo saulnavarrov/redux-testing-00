2a263306788d0964ea7fd75a31972b54ff7d0e60
Saúl Navarrov <Sinavarrov@gmail.com>
2019-11-24 09:19:53 -0500

    configuracion del switch

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,7 +3,14 @@
 // dejo por default un arreglo vacio para
 // listar los elementos
 function reducer(state = [], action) {
+  switch(action.type){
 
+    
+    
+    // Retornamos el stado normal
+    default: 
+      return state;
+  }
 }
 
 // Exportamos la funcion
