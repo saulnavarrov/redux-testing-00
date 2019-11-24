1fc5a7a0264c8401157c4db8dcfaea5ef7a2ebb3
Saúl Navarrov <Sinavarrov@gmail.com>
2019-11-24 11:18:05 -0500

    las funciones necesarias del sistema

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import store from './redux/store';
 // seleccionamos los elementos de htmlDom
 const itemsListDom = $('#itemList');
 const itemDom = $('#item');
+const txtNuevaNotaDom = $('#txtNuevaNota');
+
+// Guardamos una nueva nota al pulsar enter
+txtNuevaNotaDom.keyup((e) => {
+  if (e.keyCode === 13) {
+    // Traigo el texto de la caja de texto
+    const text = txtNuevaNotaDom.val();
+    // Reinicio la caja de texto
+    txtNuevaNotaDom.val('');
+
+    // emitimos la accion agregar
+    store.dispatch({
+      type: 'AGREGAR',
+      payload: {
+        text,
+      }
+    });
+  }
+});
 
 // funcion para actualizar listas
 function actualizarLista(items) {
@@ -23,23 +42,53 @@ function actualizarLista(items) {
     cloneItemDom.removeClass('d-none');
 
     // agregamos el texto al DOM
-    lblNombreDom.html('item.text');
+    lblNombreDom.html(item.text);
 
     // Iteramos si esta completado o no
     if (item.completado) {
       lblNombreDom.css('text-decoration','line-through');
     }
 
+    // ----------------------------------------------------
+    // Funciones de evento click
+    // ----------------------------------------------------
+    
+    // Funcion de eliminar elemento item
+    btnBorrarDom.on('click', () => {
+      // ...
+      store.dispatch({
+        type: 'BORRAR',
+        payload: {
+          idItems: item.idItems
+        }
+      });
+    });
+
+    // 
+    chkHabilitacdoDom.prop('checked', item.completado);
+    chkHabilitacdoDom.on('click', () => {
+      // ...
+      store.dispatch({
+        type: 'ALTERNAR',
+        payload: {
+          idItems: item.idItems
+        }
+      });
+    });
+
+    // Agregamos los elementos a la lista
+    itemsListDom.append(cloneItemDom);
 
   }
 }
 
 
 // cualquier cambio se vea reflejado en la pantalla
-store.subcribe(() => {
-  const state = store =getState();
+store.subscribe(() => {
+  const state = store.getState();
 
-  // defino una funcion para actualizar la listados
-  // envio los stados
+  // Defino una funcion para actualizar la lista
+  // envio los estados y los pone en pantalla
   actualizarLista(state);
 });
+
